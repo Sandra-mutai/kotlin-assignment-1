82d38169696d90ffdb3f5a6e5ba378aa8b5c6be1
sandra-mutai <sandramuttai@gmail.com>
2021-03-24 17:02:34 +0300

    second commit

--- src/assignment1.kt
+++ src/assignment1.kt
@@ -1,26 +1,12 @@
 fun main() {
-    sandra()
-    division()
-    add()
-    me()
+    println()
+
+
+    var (student)=arrayOf ("sandra,","0731405544,",38.098709,20,"true")
+    var name="sandraMutai"
+    var age=20
+    var phoneNumber="0731405544"
+    var citizen="true"
+    var weight=38.098709
+    println(student)
 }
-fun sandra(){
-    println("hello sandra")
-}
-fun division(){
-    var num1=21
-    var num2=30
-    var sum=num1/num2
-    println (sum)
-}
-fun add(){
-    var num3=4
-    var num4=6
-    var num5=12
-    var num6=10
-    var sum=num3+num4+num5+num6
-    println(sum)
-}
-fun me(){
-    var me="i am smart"
-    println(me) }
