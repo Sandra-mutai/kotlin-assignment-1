bc529dac8dd033ce8a996ffb8fb8f30362cbe967
sandra-mutai <sandramuttai@gmail.com>
2021-03-24 16:49:30 +0300

    first commit

---
+++ src/assignment1.kt
@@ -0,0 +1,26 @@
+fun main() {
+    sandra()
+    division()
+    add()
+    me()
+}
+fun sandra(){
+    println("hello sandra")
+}
+fun division(){
+    var num1=21
+    var num2=30
+    var sum=num1/num2
+    println (sum)
+}
+fun add(){
+    var num3=4
+    var num4=6
+    var num5=12
+    var num6=10
+    var sum=num3+num4+num5+num6
+    println(sum)
+}
+fun me(){
+    var me="i am smart"
+    println(me) }
